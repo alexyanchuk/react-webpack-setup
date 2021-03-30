9e47f6765892db5673e627a673309d742b7778c9
alexyanchuk <alex7devel@gmail.com>
2021-03-30 19:00:56 +0300

    first commint && setup webpack, babel

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from "./components/App";
+// Opt-in to Webpack hot module replacement
+if (module.hot) module.hot.accept()
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  )
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    open: true,
+    clientLogLevel: 'silent',
+    port: 3000,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults" 
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
+  }
+}
